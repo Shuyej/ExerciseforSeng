46c3dfe6ada35a4eeb8bcb0cd7be6ee0c86d798e
shuyej uddin <85995511+Shuyej@users.noreply.github.com>
2021-06-29 12:21:43 +0100

    Added logic of what is happening in printing the URL's

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ def main():
     #note that URLS are string type, so will user input, unless we specify user input is integer type
     soup = bs(page.content, 'html.parser')
     #print(soup)
+
     links = [] #stores all links
     for link in soup.find_all('a'): #focus on 'a' tag where each elment is refered to as link
-        links.append(link.get('href')) #update date links[] with url #meaning we use href attribute
-    for link in links:
+        links.append(link.get('href')) #update our array with elements being link, with link being URL as defined by 'get'
+#Now that information is stored on links we next print out the respective elements or data
+    for link in links: #print out elements within array
         print(link) #print each link #since last for loop updates the links, with link, we know print these elements
     print("\n")
     print("end program")
