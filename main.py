a2399cc4ef83b14b4963dccf1f22566c50483828
shuyej uddin <85995511+Shuyej@users.noreply.github.com>
2021-06-29 11:58:36 +0100

    Update with links

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ from bs4 import BeautifulSoup as bs
 
 def main():
     print("start program")
-    URL = 'https://www.bbc.co.uk/'
-    page = requests.get(URL)
+
+    #URL = 'https://www.bbc.co.uk/'
+    URLs = ['https://www.bbc.co.uk/', 'https://news.sky.com/uk',  ]
+    page = requests.get(URLs[0])
     soup = bs(page.content, 'html.parser')
+    #print(soup)
+    links = []
+    for link in soup.find_all('a'): #focus on a tag where links are store
+        links.append(link.get('href')) #update date links[] with url
+    for link in links:
+        print(link) #print each link
 
     print("end program")
 
