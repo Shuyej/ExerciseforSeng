e57d5905109349725dd53c8072965bf6a38d2f9e
shuyej uddin <85995511+Shuyej@users.noreply.github.com>
2021-07-26 14:12:01 +0100

    Implementing abstract classes on my code (continued)

--- utilities/Classes.py
+++ utilities/Classes.py
@@ -2,38 +2,46 @@ import requests
 from bs4 import BeautifulSoup as bs
 import re  # pass this library as it has variables which we will use
 from PIL import Image  # import PIL function from library Image #libraries defined outside the class
-import ABC from abc, abstractmethod
+from abc import ABC, abstractmethod
 
-class links(): #add brackets to class
+class links(ABC): #add ABC to links to make sure it is the abstract class
     #initially passed four different variables which is illogical since each URL should be a object
-    def __init__(self, URL): #want to print out all the initial statements in the initialiser, and the statements inform us all the URLs in our code
-          self.URL = str(URL)  #Declare URLS here as I want to use them in the program #And I want to specify which URL refers to which link
-
-#Use string since our URL are string
-#Inside init, otherwise known as the constructor we declare the global variables
-#URL is a global variable, and each of its elements are specified so it is globally know what its element implies
+    def __init__(self, URL):
+          self.URL = str(URL) #Any of the derive classes should expect URL
     @abstractmethod
     def getlinks(self, soup): #page and soup are defined in the main body and passed down to be executed by function for function to work
         pass
 
+    @abstractmethod
+    def getimages(self, soup):
+        pass
 
-        atag = soup.find_all('a')  # Never add this inside the for loop to be able to reuse soup.find_all('a)
-        self.links = []  # reference --> What you can get back is where info
-#Only variable soup is passed as it is used
+class BBC(links):
+    def getlinks(self, soup):
+        pass
 
-        for obj in atag:
-            href = obj.get('href') ##inside for loops we return variables
+    def getimages(self, soup):
+        pass
 
-            if href.startswith('http'): #http is a string
-                self.links.append(href) #despite links being defined outside the for loop, we add it inside, thus the variable is considered inside for loop
-#http for images
-          #  elif not href.startwith('http'): #added if statement to consider http, so for objects with no http elements we add http
-           #         self.links.append("https:" + href) #even if self.links not defined within for loop, so not noticed as a for loop variable
-                    #self.links is a local function so it is updated within the function
-            #else ##justify
-        return self.links #self.links was created as a local variable even if it wasnt passed in the paranthesis
-    #usually passing through the paranthesis is a way to declare local variables
-    #self.x is key for classes to recognise functions
+class Sky(links):
+    def getlinks(self, soup):
+        pass
+
+    def getimages(self, soup):
+        pass
+class Fox(links):
+    def getlinks(self, soup):
+        pass
+
+    def getimages(self, soup):
+        pass
+class AlJazeera(links):
+    def getlinks(self, soup):
+        pass
+
+    def getimages(self, soup):
+        pass
+
 
     def getimages(self, soup):
         images = soup.select('img') # select used as it is better to find a wider range of image sources. find_all mostly restricted to jpg or http.
